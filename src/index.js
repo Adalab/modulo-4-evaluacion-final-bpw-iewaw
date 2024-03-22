daf402484704df811111a7ddbfe562e352f72c2b
iewaw <izabelawawrzyniak90@gmail.com>
2024-03-22 19:54:52 +0100

    setting up database connection

--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 //VARIABLES
 const server = express();
 const port = 4000;
 
-//CONFIG
+//SERVER CONFIG
 server.use(cors());
 server.use(express.json({ limit: '25Mb' }));
 
-//RUN
+//RUN SERVER
 server.listen(port, () => {
     console.log(`Recipe server started at <http://localhost:${port}>`);
 })
+
+//DB CONFIG
+const getConnection = async () => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.MYSQL_HOST || 'localhost',
+            user: process.env.MYSQL_USER || 'root',
+            password: process.env.MYSQL_PASS,
+            database: process.env.MYSQL_DATABASE || 'recetas_db'
+        });
+
+        await connection.connect();
+        console.log(`Connected to database, thread id: ${connection.threadId}`);
+        return connection;
+
+    } catch (error) {
+        console.error("Error connecting to database:", error);
+        return null;
+    }
+}
+
+getConnection();
