b55a5f4a5d30049bbf6b1d318c235e031b50ff8f
iewaw <izabelawawrzyniak90@gmail.com>
2024-03-22 20:19:22 +0100

    adding an awesome recipe to data.sql

---
+++ data/data.sql
@@ -0,0 +1,16 @@
+INSERT into recetas(nombre, ingredientes, instrucciones) 
+values('Zurek - sopa polaca de harina de centeno', '200gr de salchichas, 75gr de harina de centeno, 1l de caldo de verduras, 
+300ml de nata o crema agria, 1 cebolla blanca, 100gr de champiñones, 2 dientes de ajo, 
+100gr de manteca de cerdo, 5 patatas, 2 huevos cocidos, sal al gusto, 600ml de agua',
+'Vamos a iniciar nuestra receta, fermentando nuestra harina de centeno. 
+Para ello, vamos a mezclar la mitad del agua junto al harina muy bien hasta que se disuelva. 
+La otra parte del agua la pondrás a hervir. 
+Cuando rompa hervor, se la añadirás a la mezcla anterior y revolverás hasta que se integren muy bien. 
+Tapa con papel film y dejarás reposar por 24 horas o más hasta que fermente muy bien. 
+Cuando tengas tu masa fermentada, vas a cortar la cebolla en cuadritos. 
+Calienta una cazuela con la manteca y cuando esté derretida vas a sofreír el ajo troceado y la cebolla hasta que estén blandas. 
+Luego, le vas a agregar la nata o crema agria, los champiñones, sal al gusto, el caldo y la masa fermentada. 
+Mezcla muy bien hasta que todo se integre y dejarás cocinar por 20 minutos a fuego medio. 
+Cortarás las patatas en cuadritos y las salchichas en rodajas. 
+Pasados los 20 minutos, vas a incorporar las patatas y las salchichas y dejas cocinar por 10 minutos más. 
+Apaga y sirve en una taza. Decora con el huevo duro cortado en rodajas. ¡Disfruta!');
